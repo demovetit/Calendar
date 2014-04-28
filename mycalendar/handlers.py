2fc2141a0950655653b410e337dc6a526ac4d648
demovetit <demovetit@gmail.com>
2014-04-28 16:50:09 +1000

    Colours Change Options Added

--- mycalendar/handlers.py
+++ mycalendar/handlers.py
@@ -1,5 +1,8 @@
 import mycalendar
 import mycalendar.models
+import datetime
+
+
 
 class Index(mycalendar.BaseHandler):
 
@@ -27,6 +30,7 @@ class New(mycalendar.BaseHandler):
 	def get(self):
 
 		template_values = {
+			'colours': mycalendar.models.COLOURS
 		}
 
 		self.render_template("new", template_values)
@@ -35,18 +39,19 @@ class New(mycalendar.BaseHandler):
 
 		title = self.request.get("title")
 		description = self.request.get("description")
-		date = self.request.get("date")
-		import logging
-		logging.error(type(date))
-		"""
+		datestr = self.request.get("date")
+		date = datetime.datetime.strptime(datestr, '%d-%m-%Y').date()
+		colour = self.request.get('colour')
+		
 		event = mycalendar.models.Event(
 			title = title,
 			description = description,
-			date = date
+			date = date,
+			colour = colour
 		)
 		event.put()
 		#what if its fails or user enters bad data?
 
 
 		self.redirect("/agenda")
-		"""
+		
--- mycalendar/models.py
+++ mycalendar/models.py
@@ -1,6 +1,16 @@
 from google.appengine.ext import ndb
+import datetime
+
+COLOURS = ['blue', 'green', 'red']
 
 class Event(ndb.Model):
-  title = ndb.StringProperty()
+  title = ndb.StringProperty(required = True)
   description = ndb.StringProperty()
-  date = ndb.DateProperty()
+  date = ndb.DateProperty(required = True)
+  colour = ndb.StringProperty(choices = COLOURS)
+
+  @property
+  def date_formatted(self):
+	return datetime.datetime.strftime(self.date, '%d %B %Y')
+  	
+
